6a093a30a095b0c33984ca6bdaf38e72ac5067e5
Leo Gersen <leogersen@outlook.com>
2021-02-28 08:05:34 -0300

    Incremment Hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './hooktest/Counter';
 import MouseMove from './hooktest/MouseMove';
 
 
 //ReactDOM.render(<App />,document.getElementById('root'));
-ReactDOM.render(<MouseMove />,document.getElementById('root'));
+ReactDOM.render(<Counter />,document.getElementById('root'));
 
