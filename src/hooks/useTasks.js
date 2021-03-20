668bcd155f014babeb232c160d5b2dc7879ba06c
Leo Gersen <leogersen@outlook.com>
2021-03-20 02:47:30 -0300

    Change Status

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -5,39 +5,63 @@ import { AuthContext } from "./useAuth";
 
 export const useTasks = () => {
     const auth = useContext(AuthContext);
-    const [ taskList, setTaskList ] = useState([]);
-    const [ error, setError ] = useState(null);
-    const [ processing, setProcessing ] = useState(false);
+    const [taskList, setTaskList] = useState([]);
+    const [error, setError] = useState(null);
+    const [processing, setProcessing] = useState(false);
     const [taskRemoved, setTaskRemoved] = useState(null);
+    const [taskUpdated, setTaskUpdated] = useState(null);
 
 
     const list = async () => {
 
         try {
-        setProcessing(true);
-        setError(null);
-       const response = await axios
-        .get(`${API_ENDPOINT}/tasks?sort=whenToDo,asc`, buildAuthHeader());
-        setTaskList(response.data.content);
-        setProcessing(false);
+            setProcessing(true);
+            setError(null);
+            const response = await axios
+                .get(`${API_ENDPOINT}/tasks?sort=whenToDo,asc`, buildAuthHeader());
+            setTaskList(response.data.content);
+            setProcessing(false);
 
         } catch (error) {
             handleError(error);
 
-             }
         }
+    }
 
-        
+
     const remove = async (taskToRemove) => {
-        try{
-        await axios
-        .delete(`${API_ENDPOINT}/tasks/${taskToRemove.id}`, buildAuthHeader());
-        setTaskList(taskList.filter(task => taskToRemove.id !== task.id));
-        setTaskRemoved(taskToRemove);
+        try {
+            await axios
+                .delete(`${API_ENDPOINT}/tasks/${taskToRemove.id}`, buildAuthHeader());
+            setTaskList(taskList.filter(task => taskToRemove.id !== task.id));
+            setTaskRemoved(taskToRemove);
 
-        }catch (error) {
+        } catch (error) {
             handleError(error);
 
+        }
+    }
+
+    const save = async (taskToSave, onlyStatus = false) => {
+        try {
+
+            setProcessing(!onlyStatus);
+            setTaskUpdated(null);
+            setError(null);
+
+            if (taskToSave.id === 0) {
+                await axios
+                    .post(`${API_ENDPOINT}/tasks`, taskToSave, buildAuthHeader());
+            } else {
+                await axios
+                    .put(`${API_ENDPOINT}/tasks/${taskToSave.id}`, taskToSave, buildAuthHeader());
+            }
+
+            setProcessing(false);
+            setTaskUpdated(taskToSave);
+
+        } catch (error) {
+            handleError(error);
         }
     }
 
@@ -45,7 +69,10 @@ export const useTasks = () => {
         setTaskRemoved(null);
     }
 
-    
+    const clearTaskUpdated = () => {
+        setTaskUpdated(null);
+    }
+
     const buildAuthHeader = () => {
         return {
             headers: {
@@ -67,6 +94,6 @@ export const useTasks = () => {
         setProcessing(false);
     }
 
-    return { taskList, error, processing, taskRemoved, list, remove, clearTaskRemoved } ;
-    
+    return { taskList, error, processing, taskRemoved, taskUpdated, list, remove, save, clearTaskRemoved, clearTaskUpdated };
+
 }
